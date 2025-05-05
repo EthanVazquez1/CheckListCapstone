722d7f16768855bbff99854efbf4b3e41a684d2f
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2025-05-05 15:40:55 -0700

    Add project files.

---
+++ Controllers/LibraryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckList.Controllers
+{
+    public class LibraryController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+    }
+}
